7155ddd923a6d2522c867bede77e2312225df6d8
workmint <nmabe6212@gmail.com>
2018-03-06 15:32:35 -0500

    Added TODOs to ftclient.py

--- ftclient.py
+++ ftclient.py
@@ -4,6 +4,27 @@
 #Input: port to run on (int)
 #############################
 
+#TODO: command line arg of [server host] [server port number]
+#TODO: command line support of <command>
+#TODO: command line support of <filename>
+#TODO: command line support of [data port]
+#TODO: establish connection to server
+#TODO: after connection accept args
+#TODO: support command [-l] as list all available files on new [dataport connection]
+    #TODO: SERVER see [data port] [string to print] send formatted string in [string to print] and blank [incoming filename]
+#TODO: support command [-g] [filename] as get filename
+    #TODO: [data port] connection works like this: 1st message [string to print (blank for nothing)], 2nd message [incoming file name] if blank: end connection else: file transfer to follow
+    #TODO: SERVER if [filename] is incorrect send formatted string in [string to print] and blank [incoming filename]
+    #TODO: if filename already exists, append (int counter) i to name and try again.
+#TODO: error check commands and then send an error string to server to be echoed back. Technically this fulfills the req. but not the spirit of the req. Client must display the returned error.
+#TODO: capture connections on [data port]
+#TODO: terminate after operation complete i.e. when data port is terminated, terminate.
+
+#Extra credit
+#TODO: transfer multiple file types
+#TODO: Username/Password login
+#TODO: Multi thread server....
+
 
 
 import socket as S
